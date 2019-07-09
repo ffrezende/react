d7ed0ea12e9cd18668f1f539ad180d9ad56f5307
ffrezende <fabriciofraga15@gmail.com>
2019-07-09 11:20:16 -0300

    React Hooks - Function component

--- src/features/Exemplo/views/exemplo.tsx
+++ src/features/Exemplo/views/exemplo.tsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import { Button, Grid, Paper, Typography, withStyles } from '@material-ui/core';
 
 import styles from './styles/cardStyle';
@@ -15,61 +15,54 @@ interface IState {
   loading: boolean;
 }
 
-class Exemplo extends Component<IProps, IState> {
-  constructor(props: IProps) {
-    super(props);
-    this.state = {
-      loading: false
-    };
-  }
+const Exemplo: React.FC<IProps> = (props: IProps) => {
+  const { estadoLoading, loading, classes } = props;
+  const [exemploState, setExemplo] = useState(false);
+  let estadoRedux = loading ? 'true' : 'false';
 
-  render() {
-    const { estadoLoading, loading, classes } = this.props;
-    let estadoRedux = loading ? 'true' : 'false';
-    return (
-      <div className={classes.root}>
-        <Grid
-          container
-          alignContent='center'
-          alignItems='center'
-          justify='center'
-          className={classes.gridCenter}
-        >
-          <Paper className={classes.paper}>
-            <Grid item className={classes.margin}>
-              <Button
-                variant='contained'
-                color='primary'
-                className={classes.button}
-                onClick={() => estadoLoading(true)}
-              >
-                Exemplo redux - true
-              </Button>
-              <Button
-                variant='contained'
-                color='secondary'
-                className={classes.button}
-                onClick={() => estadoLoading(false)}
-              >
-                Exemplo redux - false
-              </Button>
-              <Typography className={classes.textoPadrao}>Estado redux = {estadoRedux} </Typography>
-            </Grid>
-          </Paper>
-          <Grid item>
+  return (
+    <div className={classes.root}>
+      <Grid
+        container
+        alignContent='center'
+        alignItems='center'
+        justify='center'
+        className={classes.gridCenter}
+      >
+        <Paper className={classes.paper}>
+          <Grid item className={classes.margin}>
             <Button
               variant='contained'
               color='primary'
-              className={classes.degredeButton}
-              onClick={() => history.push('/')}
+              className={classes.button}
+              onClick={() => estadoLoading(true)}
             >
-              Voltar
+              Exemplo redux - true
             </Button>
+            <Button
+              variant='contained'
+              color='secondary'
+              className={classes.button}
+              onClick={() => estadoLoading(false)}
+            >
+              Exemplo redux - false
+            </Button>
+            <Typography className={classes.textoPadrao}>Estado redux = {estadoRedux} </Typography>
           </Grid>
+        </Paper>
+        <Grid item>
+          <Button
+            variant='contained'
+            color='primary'
+            className={classes.degredeButton}
+            onClick={() => history.push('/')}
+          >
+            Voltar
+          </Button>
         </Grid>
-      </div>
-    );
-  }
-}
+      </Grid>
+    </div>
+  );
+};
 
 export default withStyles(styles)(Exemplo);
