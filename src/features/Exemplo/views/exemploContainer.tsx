f93f4eae0c8a09947c656c4bf88b050d8438200c
ffrezende <fabriciofraga15@gmail.com>
2019-07-23 14:07:22 -0300

    exclude

--- src/features/Exemplo/views/exemploContainer.tsx
+++ src/features/Exemplo/views/exemploContainer.tsx
@@ -1,5 +1,4 @@
 import { connect } from 'react-redux';
-import { withRouter } from 'react-router-dom';
 import { AppState } from '../../../store';
 
 import * as ExemploOperation from '../redux/exemploOperations';
