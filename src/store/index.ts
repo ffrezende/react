2feb83ad8ca2fe4e0879219aaefbab5f18c4cfb5
Fabricio Fraga <root@lnb024490bhz.ciandt.global>
2020-02-11 15:52:27 -0300

    resolved bug fix on store redux

--- src/store/index.ts
+++ src/store/index.ts
@@ -3,12 +3,12 @@ import thunk from 'redux-thunk';
 
 import rootReducer from './reducers';
 
-const enhancedMiddewares: StoreEnhancer = compose(
-  applyMiddleware(thunk),
-  (window as any).__REDUX_DEVTOOLS_EXTENSION__ && (window as any).__REDUX_DEVTOOLS_EXTENSION__()
-);
+// const enhancedMiddewares: StoreEnhancer = compose(
+//   applyMiddleware(thunk),
+//   (window as any).__REDUX_DEVTOOLS_EXTENSION__ && (window as any).__REDUX_DEVTOOLS_EXTENSION__()
+// );
 
-//const enhancedMiddewares: StoreEnhancer = applyMiddleware(thunk);
+const enhancedMiddewares: StoreEnhancer = applyMiddleware(thunk);
 
 const initialState = {};
 const store: any = createStore(rootReducer, initialState, enhancedMiddewares);
