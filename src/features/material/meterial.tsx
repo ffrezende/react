8197c67647e768e54de3699c7fbab7e8fe1cd477
ffrezende <root@lnb024490bhz.ciandt.global>
2020-02-12 10:13:10 -0300

    simplification component menu

--- src/features/material/meterial.tsx
+++ src/features/material/meterial.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Drawer, Button, List, Divider, ListItem, ListItemIcon, ListItemText } from '@material-ui/core';
 import InboxIcon from '@material-ui/icons/MoveToInbox';
 import MailIcon from '@material-ui/icons/Mail';
@@ -6,47 +6,10 @@ import MailIcon from '@material-ui/icons/Mail';
 import styles from './styles/index.module.css';
 
 export default function TemporaryDrawer() {
-    const [state, setState] = React.useState({
-        left: false,
-    });
+    const [openMenu, setOpenMenu] = useState(false);
 
-    const toggleDrawer = (side: any, open: any) => (event: any) => {
-        if (event.type === 'keydown' && (event.key === 'Tab' || event.key === 'Shift')) {
-            return;
-        }
-
-        setState({ ...state, [side]: open });
-    };
-
-    const sideList = (side: any) => (
-        <div className={`${styles['list']}`} role="presentation" onClick={toggleDrawer(side, false)} onKeyDown={toggleDrawer(side, false)}>
-            <List>
-                {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
-                    <ListItem button key={text}>
-                        <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
-                        <ListItemText primary={text} />
-                    </ListItem>
-                ))}
-            </List>
-            <Divider />
-            <List>
-                {['All mail', 'Trash', 'Spam'].map((text, index) => (
-                    <ListItem button key={text}>
-                        <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
-                        <ListItemText primary={text} />
-                    </ListItem>
-                ))}
-            </List>
-        </div>
-    );
-
-    const fullList = (side: any) => (
-        <div
-            className={styles['fullList']}
-            role="presentation"
-            onClick={toggleDrawer(side, false)}
-            onKeyDown={toggleDrawer(side, false)}
-        >
+    const sideList = () => (
+        <div className={`${styles['list']}`} role="presentation">
             <List>
                 {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
                     <ListItem button key={text}>
@@ -69,9 +32,9 @@ export default function TemporaryDrawer() {
 
     return (
         <div className={`${styles['container']}`}>
-            <Button onClick={toggleDrawer('left', true)}>Open Menu</Button>
-            <Drawer open={state.left} onClose={toggleDrawer('left', false)}>
-                {sideList('left')}
+            <Button onClick={() => setOpenMenu(true)}>Open Menu</Button>
+            <Drawer open={openMenu} onClose={() => setOpenMenu(false)}>
+                {sideList()}
             </Drawer>
         </div>
     );
